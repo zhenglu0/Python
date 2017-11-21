13d564c88b9dabe4c060e6d5b479bd61a8bbc455
Zheng Luo <rogerfederer03@gmail.com>
2017-11-20 19:33:14 -0500

    Add string1.py

--- google-python-exercises/basic/string1.py
+++ google-python-exercises/basic/string1.py
@@ -25,7 +25,8 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
   # +++your code here+++
-  return
+  prefix = 'Number of donuts: '
+  return prefix + str(count) if count < 10 else prefix + 'many'
 
 
 # B. both_ends
@@ -35,7 +36,7 @@ def donuts(count):
 # is less than 2, return instead the empty string.
 def both_ends(s):
   # +++your code here+++
-  return
+  return '' if len(s) < 2 else s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -49,7 +50,7 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
   # +++your code here+++
-  return
+  return s[0] + s.replace(s[0],'*')[1:]
 
 
 # D. MixUp
@@ -61,7 +62,7 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
