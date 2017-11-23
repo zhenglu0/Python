eee8ddc671aa7dbfab00bafc204106ab69278e9b
Zheng Luo <rogerfederer03@gmail.com>
2017-11-22 19:45:54 -0500

    Add logpuzzle.py
    Description Practice the urllib material

--- google-python-exercises/logpuzzle/logpuzzle.py
+++ google-python-exercises/logpuzzle/logpuzzle.py
@@ -17,7 +17,13 @@ Given an apache logfile, find the puzzle urls and download the images.
 Here's what a puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
+protocal = 'http://'
 
+def find_suffix_pattern(url):
+  print url
+  match = re.search(r'-(\w\w\w\w).jpg', url)
+  #print match.group(1)
+  return match.group(1)
 
 def read_urls(filename):
   """Returns a list of the puzzle urls from the given log file,
@@ -25,6 +31,22 @@ def read_urls(filename):
   Screens out duplicate urls and returns the urls sorted into
   increasing order."""
   # +++your code here+++
+  hostname = filename[filename.index('_')+1:]
+  print hostname
+  try:
+    ## Either of these two lines could throw an IOError, say
+    ## if the file does not exist or the read() encounters a low level error.
+    f = open(filename, 'rU')
+    text = f.read()
+    f.close()
+    match = re.findall(r'GET (/edu/\S+.jpg) ', text)
+    urls = sorted(set(match), key = find_suffix_pattern)
+    complete_urls = [protocal+hostname+url for url in urls]
+    return complete_urls
+  except IOError:
+    ## Control jumps directly to here if any of the above lines throws IOError.
+    sys.stderr.write('problem reading:' + filename)
+
   
 
 def download_images(img_urls, dest_dir):
@@ -36,7 +58,31 @@ def download_images(img_urls, dest_dir):
   Creates the directory if necessary.
   """
   # +++your code here+++
-  
+  if not os.path.exists(dest_dir):
+    os.mkdir(dest_dir)
+
+  try:
+    suffix = 0
+    filenames = []
+    for url in img_urls:
+      filename = 'img'+str(suffix)
+      suffix += 1
+      filepath = os.path.join(dest_dir, filename)
+      # print filename
+      # print filepath
+      filenames.append(filename)
+      ufile = urllib.urlretrieve(url, filepath)
+      #print ufile
+      print 'Download', url, 'as', filename
+      # create the index.html file to diaplay the image
+      f = open(os.path.join(dest_dir,'index.html'), 'w')
+      html = '<html><body>\n'
+      for filename in filenames:
+        html += '<img src="' + filename + '">'
+      html += '\n</html></body>\n'
+      f.write(html)
+  except IOError:
+    print 'problem reading url:', url
 
 def main():
   args = sys.argv[1:]
