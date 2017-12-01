7c0da1dd162ce26c88056a789a9b7cc336be3b0b
Zheng Luo <rogerfederer03@gmail.com>
2017-12-01 17:11:27 -0500

    Add examples in test.py

--- tutorial/test.py
+++ tutorial/test.py
@@ -3,7 +3,9 @@
 import sys
 import os
 import commands
+import array
 
+### Python test read file
 def Cat(filename):
   try:
     f = open(filename, 'rU')
@@ -22,7 +24,7 @@ def Cat(filename):
   except IOError:
     print filename, "could not be opened."
 
-
+### Python execute command
 def List(dir):
   cmd = 'ls -l ' + dir
   print 'about to do this:', cmd
@@ -37,9 +39,60 @@ def List(dir):
   #   print path
   #   print os.path.abspath(path)
 
+### Python test golbal varible
+### https://www.tutorialspoint.com/python
+Money = 2000
+def AddMoney():
+   # Uncomment the following line to fix the code:
+   global Money
+   Money = Money + 1
+
 def main():
-  Cat(sys.argv[1])
-  List(sys.argv[1])
+  #Cat(sys.argv[1])
+  #List(sys.argv[1])
+  print Money
+  AddMoney()
+  print Money
+
+  # https://stackoverflow.com/questions/16887493/write-a-binary-integer-or-string-to-a-file-in-python
+  bin_array = array.array('B')
+
+  bin_array.append(int('011',2))
+  bin_array.append(int('010',2))
+  bin_array.append(int('110',2))
+
+  f = file('binary.mydata','wb')
+  bin_array.tofile(f)
+  f.close()
+
+  filename = raw_input("Enter your filename: ");
+  print "Received filename is : ", filename
+
+  # Open a file
+  f = open(filename, "wb")
+  f.write("Python is a great language.\nYeah its great!!\n 12345\n");
+
+  # Open a file
+  f = open(filename, "r+")
+  str = f.read(10);
+  print "Read String is : ", str
+
+  # Check current position
+  position = f.tell();
+  print "Current file position : ", position
+
+  # Reposition pointer at the beginning once again
+  position = f.seek(0, 0);
+  str = f.read(2);
+  print "Again read String is : ", str
+  # Close opend file
+  f.close()
+
+  # Rename a file from test1.txt to test2.txt
+  os.rename( filename, "test2.txt" )
+
+  # Delete file test2.txt
+  os.remove(os.path.join('.',"test2.txt"))
 
 if __name__ == '__main__':
   main()
